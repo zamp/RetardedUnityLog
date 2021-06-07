687c745891f0d8e632ef48cfb590eb68d83238ba
zamp <xythec@gmail.com>
2021-06-07 16:18:04 +0300

    Renamed stuff

--- ILogWriter.cs
+++ ILogWriter.cs
@@ -1,4 +1,4 @@
-namespace RetardedUnityLog
+namespace UnityLogWrapper
 {
 	public interface ILogWriter
 	{
--- Logger.cs
+++ Logger.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using UnityEngine;
 
-namespace RetardedUnityLog
+namespace UnityLogWrapper
 {
 	public enum LogLevel
 	{
